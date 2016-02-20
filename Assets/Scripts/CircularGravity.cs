45ac11e6e798900d87abc767371e578b1c3bc18d
Chad Hansen <chansen.306@gmail.com>
2016-02-20 10:32:55 -0600

    Added basic gravity that pushes player to the closest edge of the circle. Added circle collider to player's feet so he can run around, but he needs to rotate to face his feet towards the ground. Working on that now.

---
+++ Assets/Scripts/CircularGravity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircularGravity : MonoBehaviour {
+	public float gravityStrength;
+
+	Vector2 CENTER = new Vector2(0, 0);
+	Rigidbody2D objRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		objRigidBody = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate(){
+		Vector2 gravityDirection = gameObject.transform.position.normalized;
+		objRigidBody.AddForce( gravityStrength*gravityDirection );
+	}
+
+}
