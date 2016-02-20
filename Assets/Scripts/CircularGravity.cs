58499bd66195ebfc7118d9dc23297abc1ecbd2a5
Chad Hansen <chansen.306@gmail.com>
2016-02-20 11:51:57 -0600

    Added rotation of player (assumes script is attached to player, can generalize this later) to face the correct surface of the circle. Currently we assume the circle is centered at 0,0 for simplicity.

--- Assets/Scripts/CircularGravity.cs
+++ Assets/Scripts/CircularGravity.cs
@@ -2,9 +2,12 @@
 using System.Collections;
 
 public class CircularGravity : MonoBehaviour {
+	GameObject parent;
 	public float gravityStrength;
-
-	Vector2 CENTER = new Vector2(0, 0);
+	/*Player starts with 0 degrees rotation meaning his feet point down.
+	  We need to add that to the calculation for the direction to point his feet.
+	*/
+	private const float PLAYER_ANGLE_CORRECTION = 90f;
 	Rigidbody2D objRigidBody;
 
 	// Use this for initialization
@@ -15,6 +18,11 @@ public class CircularGravity : MonoBehaviour {
 	void FixedUpdate(){
 		Vector2 gravityDirection = gameObject.transform.position.normalized;
 		objRigidBody.AddForce( gravityStrength*gravityDirection );
+		rotateBody( gravityDirection );
 	}
 
+	private void rotateBody(Vector2 gravityDirection){
+		float angleForFeet = Mathf.Atan2( gravityDirection.y, gravityDirection.x )*180f/Mathf.PI + PLAYER_ANGLE_CORRECTION;
+		gameObject.transform.eulerAngles = new Vector3( 0, 0, angleForFeet );
+	}
 }
