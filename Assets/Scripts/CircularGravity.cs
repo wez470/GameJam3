14fec2f1ff2eb1741e31eecabea8dc475cdf2b3b
Chad Hansen <chansen.306@gmail.com>
2016-02-20 12:26:03 -0600

    Add velocity left or right of what is 'up' relative to the player. Needs some parameter tweaking.

--- Assets/Scripts/CircularGravity.cs
+++ Assets/Scripts/CircularGravity.cs
@@ -4,10 +4,6 @@ using System.Collections;
 public class CircularGravity : MonoBehaviour {
 	GameObject parent;
 	public float gravityStrength;
-	/*Player starts with 0 degrees rotation meaning his feet point down.
-	  We need to add that to the calculation for the direction to point his feet.
-	*/
-	private const float PLAYER_ANGLE_CORRECTION = 90f;
 	Rigidbody2D objRigidBody;
 
 	// Use this for initialization
@@ -18,11 +14,5 @@ public class CircularGravity : MonoBehaviour {
 	void FixedUpdate(){
 		Vector2 gravityDirection = gameObject.transform.position.normalized;
 		objRigidBody.AddForce( gravityStrength*gravityDirection );
-		rotateBody( gravityDirection );
-	}
-
-	private void rotateBody(Vector2 gravityDirection){
-		float angleForFeet = Mathf.Atan2( gravityDirection.y, gravityDirection.x )*180f/Mathf.PI + PLAYER_ANGLE_CORRECTION;
-		gameObject.transform.eulerAngles = new Vector3( 0, 0, angleForFeet );
 	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,11 @@ using System.Collections;
 public class Player : MonoBehaviour {
 	private const float ROT_DEAD_ZONE = 0.2f;
 
+	/*Player starts with 0 degrees rotation meaning his feet point down.
+	  We need to add that to the calculation for the direction to point his feet.
+	*/
+	private const float PLAYER_ANGLE_CORRECTION = 90f;
+
 	public float Speed;
 
 	private int playerNum = 1;
@@ -19,7 +24,19 @@ public class Player : MonoBehaviour {
 	}
 
 	private void setMovement() {
-		float speedX = XCI.GetAxis(XboxAxis.LeftStickX, playerNum) * Speed;
-		GetComponent<Rigidbody2D>().velocity = new Vector2(speedX, GetComponent<Rigidbody2D>().velocity.y);
+		Vector2 down = gameObject.transform.position.normalized;
+		Vector2 left = new Vector2( -down.y, down.x );
+		Vector2 currentVelocity = GetComponent<Rigidbody2D>().velocity;
+
+		rotateBody( down );
+
+		Vector2 velocity = ( XCI.GetAxis(XboxAxis.LeftStickX, playerNum) * Speed * left);
+
+		GetComponent<Rigidbody2D>().velocity = currentVelocity + velocity;
+	}
+
+	private void rotateBody(Vector2 gravityDirection){
+		float angleForFeet = Mathf.Atan2( gravityDirection.y, gravityDirection.x )*180f/Mathf.PI + PLAYER_ANGLE_CORRECTION;
+		gameObject.transform.eulerAngles = new Vector3( 0, 0, angleForFeet );
 	}
 }
