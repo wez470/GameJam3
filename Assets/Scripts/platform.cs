f2bbb32b833166fdc69858826b2532dddf013408
Stephanie Dilsner (ssd843) <ssd843@mail.usask.ca>
2016-02-20 17:58:17 -0600

    platforms

---
+++ Assets/Scripts/platform.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platform : MonoBehaviour
+{
+    float yRotation = 5.0F;
+    public float rotationSpeed = 1.0F;
+    void Update()
+    {
+        //yRotation += yRotation;
+        yRotation += rotationSpeed;
+        transform.eulerAngles = new Vector3(10, 0, yRotation);
+    }
+}
