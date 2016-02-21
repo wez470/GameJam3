1bf499e4a81578c0729265e3fafa8bb395d605cd
Chad Hansen <chansen.306@gmail.com>
2016-02-20 19:45:30 -0600

    Rings randomly switch direction and speed now.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,41 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+
+	public float ROTATION_DELTA;
+	public float CurrentTime;
+
 	void Start () {
 		Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Bullet"), LayerMask.NameToLayer("Terrain"));
 		Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Bullet"), LayerMask.NameToLayer("Bullet"));
+
+		CurrentTime = Time.timeSinceLevelLoad;
+	}
+
+	void Update(){
+
+		if (Time.time >= (CurrentTime + ROTATION_DELTA) ){
+			CurrentTime = Time.time;
+
+			rotatePlatforms( "InnerPlatforms", getRandomRotationSpeed() );
+			rotatePlatforms( "OuterPlatforms", getRandomRotationSpeed() );
+		}
+	}
+
+	private void rotatePlatforms( string platformTag, float rotationSpeed ){
+		GameObject[] platforms = GameObject.FindGameObjectsWithTag( platformTag );
+
+			for (int i = 0; i < platforms.Length; i++){
+				platform p = platforms[i].GetComponent<platform>();
+				p.rotationSpeed = rotationSpeed;
+			}
+	}
+
+	private float getRandomRotationSpeed(){
+		float val = UnityEngine.Random.Range( 1, 3);
+		if (UnityEngine.Random.value > 0.5f){
+			val *= -1;
+		}
+		return val;
 	}
 }
