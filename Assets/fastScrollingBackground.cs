513ad06885f6de7df6bb5e66fb4427fce3f244e4
Joel Kesler <joelkesler@gmail.com>
2016-02-21 02:40:16 -0600

    Added Title Graphics and Rough Menu Idea

---
+++ Assets/fastScrollingBackground.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fastScrollingBackground : MonoBehaviour
+{
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // print("here");
+        //float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        startPosition.y += 0.5f;
+        transform.position = startPosition;
+		if (startPosition.y > 46.4f)
+        {
+			startPosition.y = -46.4f;
+        }
+    }
+}
