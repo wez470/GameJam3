4b4f75f76ab5f2be27785db0826c92ec54f39f5b
Stephanie Dilsner (ssd843) <ssd843@mail.usask.ca>
2016-02-21 11:58:07 -0600

    title screen to play

---
+++ Assets/startGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using XboxCtrlrInput;
+using System.Collections;
+using System;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour
+{ 
+    void Update()
+    {
+        for (int i = 1; i <= XCI.GetNumPluggedCtrlrs(); i++)
+        {
+            if (XCI.GetButtonDown(XboxButton.Start, i))
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+
+}
+
