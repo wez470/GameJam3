19805186497297f150d01822937ea3b66d8c94db
Stephanie Dilsner (ssd843) <ssd843@mail.usask.ca>
2016-02-21 01:04:56 -0600

    background scroll

---
+++ Assets/rotateBackground.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateBackground : MonoBehaviour 
+{
+    float yRotation = 10.0F;
+    public float rotationSpeed = 10.0F;
+
+    void Update()
+    {
+        //spin
+        yRotation += rotationSpeed;
+        transform.eulerAngles = new Vector3(10, 0, yRotation);
+    }
+}
---
+++ Assets/scrollingBackground.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrollingBackground : MonoBehaviour
+{
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // print("here");
+        //float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        startPosition.x += 0.6f;
+        transform.position = startPosition;
+        if (startPosition.x > 47.3f)
+        {
+            startPosition.x = -47.3f;
+        }
+    }
+}
