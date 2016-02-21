5d0e32a123f7c21068d91f6202a5d16388ee1eea
Joel Kesler <joelkesler@gmail.com>
2016-02-21 16:29:59 -0600

    Sweetened up the menu

--- Assets/startGame.cs
+++ Assets/startGame.cs
@@ -7,17 +7,38 @@ using UnityEngine.SceneManagement;
 public class startGame : MonoBehaviour
 {
     public AudioSource buttonSound;
+	public Animator MenuAnimator;
+
+
     void Update()
-    {
-        for (int i = 1; i <= XCI.GetNumPluggedCtrlrs(); i++)
-        {
-            if (XCI.GetButtonDown(XboxButton.Start, i))
-            {
-                SceneManager.LoadScene(1);
-                buttonSound.Play();
-            }
-        }
-    }
+	{
+
+		for (int i = 1; i <= XCI.GetNumPluggedCtrlrs(); i++)
+		{
+			if (XCI.GetButtonDown(XboxButton.Start, i))
+			{
+				buttonSound.Play ();
+				MenuAnimator.SetBool("canplayMenuAnimation", true);
+				Invoke ("startGameNow", 1.4f);
+			}
+		}
+
+
+		if (Input.GetKeyDown ("return")) {
+			buttonSound.Play ();
+			MenuAnimator.SetBool("canplayMenuAnimation", true);
+			Invoke ("startGameNow", 1.4f);
+
+		}
+	}
+
+	void startGameNow(){
+			SceneManager.LoadScene(1);
+	}
+
+
+       /* 
+    }*/
 
 }
 
