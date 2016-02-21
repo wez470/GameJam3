bcb222c4a48d423b98a4ed9678a2abc85ca381a9
Weston <wez470@gmail.com>
2016-02-20 23:04:49 -0600

    Cleanup turret gun script

--- Assets/Scripts/TurretGun.cs
+++ Assets/Scripts/TurretGun.cs
@@ -4,13 +4,7 @@ using System.Collections;
 public class TurretGun : MonoBehaviour {
 	public Animator GunAnimator;
 
-	public void Test() {
-		Debug.Log("TEST");
-		GetComponent<Animator>().SetBool("shoot", false);
-	}
-
 	public void StopShooting() { 
-		Debug.Log("SET FALSE");
 		GunAnimator.SetBool("shoot", false);
 	}
 }
